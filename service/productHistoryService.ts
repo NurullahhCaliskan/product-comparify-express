f727c0296437cf2312827f2e6be66d7f94bf8be5
nurul <nurullahhcaliskan@gmail.com>
2022-06-22 14:20:58 +0300

    update engine

--- service/productHistoryService.ts
+++ service/productHistoryService.ts
@@ -59,6 +59,8 @@ export default class ProductHistoryService {
                 }
             }
         }
+
+        await productHistoryRepository.saveProductsFromWebByUrl(products)
     }
 
     /***
