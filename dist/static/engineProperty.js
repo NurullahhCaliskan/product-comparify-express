edf1c7ae8abc89f4f80881d157a43a21b73359c5
nurul <nurullahhcaliskan@gmail.com>
2022-08-02 14:09:55 +0300

    update enginedas

---
+++ dist/static/engineProperty.js
@@ -0,0 +1,19 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.setEngineEndDate = exports.setEngineStartDate = exports.endDate = exports.startDate = void 0;
+exports.startDate = new Date();
+exports.endDate = new Date();
+/***
+ * set Start date to engine
+ */
+function setEngineStartDate() {
+    exports.startDate = new Date();
+}
+exports.setEngineStartDate = setEngineStartDate;
+/***
+ * set End date to engine
+ */
+function setEngineEndDate() {
+    exports.endDate = new Date();
+}
+exports.setEngineEndDate = setEngineEndDate;
--- static/engineProperty.ts
+++ static/engineProperty.ts
@@ -1,10 +1,16 @@
 export let startDate: Date = new Date();
 export let endDate: Date = new Date();
 
+/***
+ * set Start date to engine
+ */
 export function setEngineStartDate() {
     startDate = new Date();
 }
 
+/***
+ * set End date to engine
+ */
 export function setEngineEndDate() {
     endDate = new Date();
 }
